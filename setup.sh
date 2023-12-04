6e6f0980c04301bb5daeb86236986ef3b26cd014
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-12-04 16:58:51 +0900

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Change udev-rules to allow access to the device
 
+touch /etc/udev/rules.d/usb.rules
 grep -F "SUBSYSTEM=="usb", MODE="0666"" /etc/udev/rules.d/usb.rules ||
 sudo echo "SUBSYSTEM=="usb", MODE="0666" >> /etc/udev/rules.d/usb.rules
 
